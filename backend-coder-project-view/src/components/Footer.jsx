a438d37fe1c3b45e33325efc4fa20c1a42611c61
santio89 <santiolais1989@gmail.com>
2022-11-30 12:44:19 -0300

    small fix

--- backend-coder-project-view/src/components/Footer.jsx
+++ backend-coder-project-view/src/components/Footer.jsx
@@ -1,14 +1,13 @@
 import { Link } from 'react-router-dom';
 import '../styles/css/Footer.css';
 
-const Footer = ({darkTheme, toggleDarkTheme}) => {
+const Footer = ({ darkTheme, toggleDarkTheme }) => {
 
     return (
         <footer className='footer'>
-            <p>Footer</p>
             <div className='theme-mode'>
                 <div className='theme-mode_inner'>
-                    <input type="checkbox" className="checkbox" id="checkbox" onChange={toggleDarkTheme} checked={darkTheme && true} aria-label="light/dark theme - input"/>
+                    <input type="checkbox" className="checkbox" id="checkbox" onChange={toggleDarkTheme} checked={darkTheme && true} aria-label="light/dark theme - input" />
                     <label htmlFor="checkbox" className="label-dm" aria-label="light/dark theme - label">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="moon-fill" viewBox="0 0 16 16">
                             <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z" />
@@ -21,6 +20,12 @@ const Footer = ({darkTheme, toggleDarkTheme}) => {
                     </label>
                 </div>
             </div>
+            
+            <Link to='/'>
+                <p>MOVIESTORE</p>
+            </Link>
+            
+            <a href='https://santiweb.netlify.app/' className='footer__santi'>santiWeb</a> 
         </footer>
     );
 };
